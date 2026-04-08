c5cbe441b281a38c878377fba2401876fb0288a1
CUBE4927 <98718163+CUBE4927@users.noreply.github.com>
2026-04-08 11:51:36 +0900

    Update playback.js

--- ScaleSystem/Table/playback.js
+++ ScaleSystem/Table/playback.js
@@ -19,3 +19,32 @@ function stopPlayback() {
   });
   activeOscillators = [];
 }
+
+function playCurrentMode(accidentalText) {
+  const ctx = ensureAudioContext();
+  stopPlayback();
+
+  console.log('PLAY:', accidentalText);
+
+  // 임시 테스트용 단일음
+  const osc = ctx.createOscillator();
+  const gain = ctx.createGain();
+
+  osc.type = 'sine';
+  osc.frequency.value = 440;
+
+  gain.gain.setValueAtTime(0, ctx.currentTime);
+  gain.gain.linearRampToValueAtTime(0.08, ctx.currentTime + 0.02);
+  gain.gain.linearRampToValueAtTime(0, ctx.currentTime + 0.8);
+
+  osc.connect(gain);
+  gain.connect(ctx.destination);
+
+  osc.start(ctx.currentTime);
+  osc.stop(ctx.currentTime + 0.85);
+
+  activeOscillators.push({ osc, gain });
+}
+
+window.playCurrentMode = playCurrentMode;
+window.stopPlayback = stopPlayback;
